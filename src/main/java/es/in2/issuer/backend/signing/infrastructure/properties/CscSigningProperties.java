b18ad6582986ee0def48a300d8d1a2895628ad5c
Alba Lopez <alba.lopez@in2.es>
2026-02-25 14:04:32 +0100

    feat: add TODO comment for configuration in CscSigningProperties

--- src/main/java/es/in2/issuer/backend/signing/infrastructure/properties/CscSigningProperties.java
+++ src/main/java/es/in2/issuer/backend/signing/infrastructure/properties/CscSigningProperties.java
@@ -4,12 +4,12 @@ import es.in2.issuer.backend.signing.domain.model.JadesProfile;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
 
-@ConfigurationProperties(prefix = "issuer.signing.csc")
+@ConfigurationProperties(prefix = "issuer.signing.csc") //TODO: add config
 @Validated
 public record CscSigningProperties(
         JadesProfile signatureProfile
 ) {
     public CscSigningProperties {
-        if (signatureProfile == null) signatureProfile = JadesProfile.JADES_B_T; // default recomendado
+        if (signatureProfile == null) signatureProfile = JadesProfile.JADES_B_T;
     }
 }
